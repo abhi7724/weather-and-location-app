a6e888cb8b2ba712eabf9619ffd4fd3d79ae9c84
ABHINAV <abhinav30may@gmail.com>
2022-10-25 10:38:36 +0530

    refracted getcurrentlocation

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,50 +1,27 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:weatherapp/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
-
-
-
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
-
-
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation()
-  async{
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      return Future.error('Location services are disabled');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-  print(position);
+  @override
+  void initState(){
+    super.initState();
+    getLocation();
+  }
+  void getLocation() async{
+    Location location = Location();
+    await location.getCurrentLocation(); // for using this function inthe class we have to make the function await
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-
-
-
       body: Center(
         child: ElevatedButton(
           onPressed: () {
